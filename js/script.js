ee08fc25fbd40952a4eb31a84f0ca8c30237318b
david <dyoesmawan@gmail.com>
2025-03-13 19:31:56 +0900

    feat: achievement & profile, css update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 //  ini file js yang akan dijalankan pada halaman index.php
 
+// untuk menampilkan nama pengunjung
+function visitorName(){
+    let name = prompt("What is your name?", "");
+    if(name != null){
+        document.getElementById("visitor-name").innerHTML = name;
+    }
+}
+visitorName();
+
+//untuk menggerakkan banner autoslide
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n){
+    showDivs(slideIndex += n);
+}
+function plusDivdiv(n){
     showDivs(slideIndex += n);
 }
 function showDivs(n){
@@ -21,3 +34,48 @@ function showDivs(n){
 setInterval(() => {
     plusDivs(1);
 }, 1500); //1500 milidetik = 1,5 detik
+
+//mengganti gambar slide ketika ditekan
+let bannerPress = document.getElementById("main-banner");
+bannerPress.addEventListener("click", function() {
+    plusDivdiv(1);
+});
+
+
+//untuk menampilkan form
+function previewForm(){
+    document.getElementById("Nama").innerHTML = "John Doe";
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == ""){
+        alert("Name must be filled out");
+        return false;
+    }
+    else{
+        alert("Welcome, " + x + "!");
+    }
+} 
+
+//validasi form
+function validateForm(){
+    const name= document.forms["message-form"]["name"].value;
+    const birthDate= document.forms["message-form"]["tgl_lahir"].value;
+    const gender= document.forms["message-form"]["jenis_kelamin"].value;
+    const message= document.forms["message-form"]["message"].value;
+
+    if(name == "" || birthDate == "" || gender == "" || message == ""){
+        alert("Tidak boleh ada data kosong");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, message);
+
+    return false;
+}
+
+//menampilkan hasil submit
+function setSenderUI(name, birthDate, gender, message){
+    document.getElementById("sender-fullname").innerHTML = name;
+    document.getElementById("sender-birthdate").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-message").innerHTML = message;
+}
